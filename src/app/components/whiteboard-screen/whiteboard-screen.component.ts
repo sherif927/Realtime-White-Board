e61aa5952512d20c39ea7dbdd545c858810b5357
Sherif Amr <sherif.amr.927@gmail.com>
2020-05-09 00:24:49 +0200

    provided documentation for the whiteboard component

--- src/app/components/whiteboard-screen/whiteboard-screen.component.ts
+++ src/app/components/whiteboard-screen/whiteboard-screen.component.ts
@@ -22,6 +22,12 @@ export class WhiteboardScreenComponent implements OnInit, AfterViewInit {
 
   ngOnInit(): void { }
 
+  /**
+   * initialize the canvas context obj
+   * and setup the event handlers
+   *
+   * @memberof WhiteboardScreenComponent
+   */
   ngAfterViewInit(): void {
     this.ctx = this.myCanvas.nativeElement.getContext('2d');
     this.resizeCanvas();
@@ -30,10 +36,24 @@ export class WhiteboardScreenComponent implements OnInit, AfterViewInit {
     this.myCanvas.nativeElement.addEventListener('mousemove', this.draw);
   }
 
+
+  /**
+   * select/unselect the eraser
+   *
+   * @memberof WhiteboardScreenComponent
+   */
   toggleEraser(): void {
     this.eraser = !this.eraser;
   }
 
+
+  /**
+   * methods passed for the event-handler
+   * are passed as arrow functions to avoid
+   * conflicts over the 'this' keyword.
+   *
+   * @memberof WhiteboardScreenComponent
+   */
   startPainting = (e) => {
     this.painting = true;
     this.ctx.beginPath();
@@ -45,6 +65,15 @@ export class WhiteboardScreenComponent implements OnInit, AfterViewInit {
     this.painting = false;
   }
 
+
+  /**
+   * this method is called when the mouse
+   * is dragged , it checks whether the user
+   * is selecting the eraser of just the normal
+   * pen and draws accordingly.
+   *
+   * @memberof WhiteboardScreenComponent
+   */
   draw = (e) => {
     if (!this.painting) return;
     if (this.eraser)
@@ -55,12 +84,27 @@ export class WhiteboardScreenComponent implements OnInit, AfterViewInit {
     this.ctx.stroke();
   }
 
+
+
+  /**
+   * sets the properties of the pen
+   * such as pen width , color and style.
+   *
+   * @memberof WhiteboardScreenComponent
+   */
   setProperties(): void {
     this.ctx.lineWidth = this.strokeWidth;
     this.ctx.lineCap = this.lineStyle;
     this.ctx.strokeStyle = this.strokeColor;
   }
 
+
+  /**
+   * sets some static properties to mimic
+   * the effect of an eraser. 
+   *
+   * @memberof WhiteboardScreenComponent
+   */
   setEraserProperties(): void {
     this.ctx.lineWidth = 15;
     this.ctx.lineCap = this.lineStyle;
@@ -72,20 +116,47 @@ export class WhiteboardScreenComponent implements OnInit, AfterViewInit {
     this.myCanvas.nativeElement.height = window.innerHeight;
   }
 
+
+  /**
+   * decrements the value of the pen's width
+   * but makes sure it's always bigger than 1
+   *
+   * @memberof WhiteboardScreenComponent
+   */
   decrementRadius(): void {
     this.strokeWidth -= (this.strokeWidth > 1) ? 1 : 0;
   }
 
+  /**
+   * increments the value of the pen's width
+   * but makes sure it's never bigger than 25
+   *
+   * @memberof WhiteboardScreenComponent
+   */
   incrementRadius(): void {
     this.strokeWidth += (this.strokeWidth < 25) ? 1 : 0;
   }
 
+
+  /**
+   * selects the color and if the eraser
+   * is selected it unselects it.
+   *
+   * @param {number} index
+   * @memberof WhiteboardScreenComponent
+   */
   selectColor(index: number): void {
     if (this.eraser) this.eraser = false;
     this.selectedColor = index;
     this.strokeColor = this.colors[this.selectedColor];
   }
 
+  /**
+   * if the dimensions of the canvas are
+   * changed, then it's re-created
+   *
+   * @memberof WhiteboardScreenComponent
+   */
   clearScreen(): void {
     this.myCanvas.nativeElement.width = window.innerWidth;
   }
